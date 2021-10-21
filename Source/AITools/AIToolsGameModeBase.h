393fe9e41cce164b9e0bf0a260ed32e1598b63f1
predbannikov <predbannikov.yurii@gmail.com>
2021-10-21 23:23:08 +0300

    first commit

---
+++ Source/AITools/AIToolsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AIToolsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AITOOLS_API AAIToolsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
